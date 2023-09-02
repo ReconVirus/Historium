d7540eedc86b8f9656cf8adede176cb0a7709993
ReconVirus <43733760+ReconVirus@users.noreply.github.com>
2023-09-02 14:13:13 -0700

    Initial
    Development Starting point

---
+++ rollup.config.js
@@ -0,0 +1,21 @@
+import typescript from '@rollup/plugin-typescript';
+import {nodeResolve} from '@rollup/plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs';
+import styles from "rollup-plugin-styles";
+
+export default {
+    input: './src/Main.ts',
+    output: {
+        dir: '.',
+        sourcemap: 'inline',
+        format: 'cjs',
+        exports: 'default'
+    },
+    external: ['obsidian', 'crypto'],
+    plugins: [
+        typescript(),
+        styles(),
+        nodeResolve({browser: true}),
+        commonjs(),
+    ]
+};
